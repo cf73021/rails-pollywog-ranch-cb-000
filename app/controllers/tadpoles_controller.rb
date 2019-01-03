79bd2004a4444b4ab65bfff518a8dbf7848145f8
Chris Franklin <cf73021@gmail.com>
2019-01-03 18:00:24 +0000

    Automatically backed up by Learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,7 +2,7 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
-    @new = Frog.new(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
+    @new = Frog.create(name: @tadpole.name, color: @tadpole.color, pond: @tadpole.pond)
     Tadpole.destroy(@tadpole)
     redirect_to frog_path(@new)
   end
